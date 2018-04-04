2a90d6673886402f852c3b73a3c7503f525dec84
Koh Wei Jie <contact@kohweijie.com>
2018-04-05 06:50:43 +0800

    deploy

--- backend/queryCmc.js
+++ backend/queryCmc.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 
 
 const queryCmc = async db => {
-  fs.readFile('./sample.json', function (err, data) {
-    db.updateCoins(JSON.parse(data))
-  })
+  //fs.readFile('./sample.json', function (err, data) {
+    //db.updateCoins(JSON.parse(data))
+  //})
 
-  //const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=0'
-  //const response = await fetch(url)
-  //const coins = await response.json()
-  //console.log('Fetched data for ' + coins.length.toString() + ' coins')
-  //await db.updateCoins(coins)
+  const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=0'
+  const response = await fetch(url)
+  const coins = await response.json()
+  console.log('Fetched data for ' + coins.length.toString() + ' coins')
+  await db.updateCoins(coins)
 }
 
 module.exports = queryCmc
--- build/backend/queryCmc.js
+++ build/backend/queryCmc.js
@@ -7,21 +7,32 @@ var fs = require('fs');
 
 var queryCmc = function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(db) {
+    var url, response, coins;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            fs.readFile('./sample.json', function (err, data) {
-              db.updateCoins(JSON.parse(data));
-            });
+            //fs.readFile('./sample.json', function (err, data) {
+            //db.updateCoins(JSON.parse(data))
+            //})
 
-            //const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=0'
-            //const response = await fetch(url)
-            //const coins = await response.json()
-            //console.log('Fetched data for ' + coins.length.toString() + ' coins')
-            //await db.updateCoins(coins)
+            url = 'https://api.coinmarketcap.com/v1/ticker/?limit=0';
+            _context.next = 3;
+            return fetch(url);
 
-          case 1:
+          case 3:
+            response = _context.sent;
+            _context.next = 6;
+            return response.json();
+
+          case 6:
+            coins = _context.sent;
+
+            console.log('Fetched data for ' + coins.length.toString() + ' coins');
+            _context.next = 10;
+            return db.updateCoins(coins);
+
+          case 10:
           case 'end':
             return _context.stop();
         }
